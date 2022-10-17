fa51ff735ce94dcfc5b213342daa8167bab7003e
irhesri <rhesri.1337@gmail.com>
2022-10-17 19:38:55 +0100

    master

--- parsing/expand.c
+++ parsing/expand.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   expand.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: irhesri <irhesri@student.42.fr>            +#+  +:+       +#+        */
+/*   By: imane <imane@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/07 21:10:09 by irhesri           #+#    #+#             */
-/*   Updated: 2022/10/17 16:18:53 by irhesri          ###   ########.fr       */
+/*   Updated: 2022/10/17 19:32:33 by imane            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,7 +80,7 @@ char	**split_expand(char *str, char *tmp, int *len)
 	res = var_expand(str, len);
 	b[0] = (res && res[0] == ' ');
 	b[1] = 0;
-	if (str[*len] && is_limiter(str + *len) > 2 && res && res[my_size(NULL, res) - 1] == ' ')
+	if (str[*len] && !(is_limiter(str + *len) > 2) && res && res[my_size(NULL, res) - 1] == ' ')
 		b[1] = 1;
 	str = res;
 	while (str && *str)
